d3625a32a7a480c2b9c937af633aca7fc21d629d
gemma-serra <serradepazgemma@gmail.com>
2022-02-27 12:20:43 +0100

    exercici ping pong

--- src/ros_basics/scripts/pong.py
+++ src/ros_basics/scripts/pong.py
@@ -4,11 +4,13 @@ from std_msgs.msg import String
 
 def pong_callback(msg):
     pong_msg = String()
-    
-    pong_msg.data = "Pong"
+    if (msg.data == "Ping"):
+        pong_msg.data = "Pong"
+    else:
+        pong_msg.data = "Failed!"
     pub.publish(pong_msg)
     rospy.loginfo(pong_msg.data)
-    
+
 rospy.init_node('pong_node')
 sub = rospy.Subscriber('/ping', String, pong_callback)
 pub = rospy.Publisher('/pong', String, queue_size=10)
